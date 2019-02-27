6e11d388524f3229e8fa2dc5fd76f26b33d780de
OlayG <ogureje@yahoo.com>
2019-02-26 21:08:19 -0500

    sorry was missing some layouts

--- app/src/main/java/com/noname/layouts/MainActivity.java
+++ app/src/main/java/com/noname/layouts/MainActivity.java
@@ -2,12 +2,24 @@ package com.noname.layouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.linear_layout_example);
+
+        Button login = findViewById(R.id.btn_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
